4109069f26b423f942119e9a5196d5b26fcabed2
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-06-28 04:45:55 +0700

    add api config

---
+++ src/api/requests.js
@@ -0,0 +1,12 @@
+/** @format */
+
+// function to fetch fakestoreapi
+const request = {
+	fetchAll: `/products`,
+	fetchWomenClothing: `/products/category/women's clothing`,
+	fetchMenClothing: `/products/category/men's clothing`,
+	fetchJewelery: `/products/category/jewelery`,
+	fetchElectronics: `/products/category/electronics`,
+};
+
+export default request;
