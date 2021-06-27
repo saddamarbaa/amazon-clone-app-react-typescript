ced789994f16cc250db46a9ba8fd009ea01d9d46
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-06-28 04:50:31 +0700

    feat: add Banner component with Image Carousel

---
+++ src/components/Banner.js
@@ -0,0 +1,75 @@
+/** @format */
+
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import styled from "styled-components";
+
+const Banner = () => {
+	return (
+		<Wrapper>
+			<BannerContainer>
+				<Carousel
+					infiniteLoop
+					autoPlay
+					showStatus={false}
+					showIndicators={false}
+					showThumbs={false}
+					interval={2000}>
+					<div>
+						<img src='/images/banner3.jpg' alt='amazon banner' />
+					</div>
+					<div>
+						<img src='/images/banner5.jpg' alt='amazon banner' />
+					</div>
+					<div>
+						<img src='/images/banner4.jpg' alt='amazon banner' />
+					</div>
+
+					<div>
+						<img src='/images/banner.jpg' alt='amazon banner' />
+					</div>
+
+					<div>
+						<img src='/images/banner2.jpg' alt='amazon banner' />
+					</div>
+				</Carousel>
+			</BannerContainer>
+		</Wrapper>
+	);
+};
+
+export default Banner;
+
+const Wrapper = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+`;
+
+const BannerContainer = styled.div`
+	z-index: -3 !important;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	margin-right: auto;
+	margin-left: auto;
+	max-width: 1500px;
+	margin-top: -10px;
+	margin-bottom: -25% !important;
+
+	img {
+		width: 100%;
+		max-width: 100%;
+		max-height: 100%;
+		z-index: -3 !important;
+		object-fit: contain;
+		mask-image: linear-gradient(
+			to bottom,
+			rgba(0, 0, 0, 1),
+			rgba(0, 0, 0, 1),
+			rgba(0, 0, 0, 0),
+			rgba(0, 0, 0, 0)
+		);
+	}
+`;
