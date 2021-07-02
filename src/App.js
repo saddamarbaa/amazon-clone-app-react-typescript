ad0169328543c70adff4f453449bb4b587c07229
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-07-03 01:14:34 +0700

    add more products Images and data

--- src/App.js
+++ src/App.js
@@ -6,21 +6,89 @@ import styled from "styled-components";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Checkout from "./components/CheckoutProduct";
+import Footer from "./components/Footer";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import { auth } from "./config/firebase";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	setLogInState,
+	setLogOutState,
+	selectUser,
+} from "./features/user/userSlice";
+import Payment from "./components/Payment";
+import Order from "./components/Order";
 
 function App() {
+	const dispatch = useDispatch();
+	const user = useSelector(selectUser); // grab the user from global state
+
+	// keep user login In
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((authenticatedUser) => {
+			if (authenticatedUser) {
+				// User is signed in / The User was Logged in,
+				// dispatch an action and change the state(save user in the global store)
+
+				dispatch(
+					setLogInState({
+						displayName: authenticatedUser.displayName
+							? authenticatedUser.displayName
+							: "",
+						uid: authenticatedUser.uid,
+						email: authenticatedUser.email,
+					}),
+				);
+				// console.log("authenticated is User object", authenticatedUser);
+			} else {
+				// User is signed out
+				// dispatch an action and change the state(remove a user from  the global store)
+				dispatch(setLogOutState());
+			}
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	}, [dispatch]);
+
 	return (
 		<AppWrapper>
 			<Router>
-				<Header />
-				<Switch>
-					<Route path='/' exact>
-						<Home />
-					</Route>
+				{!user ? (
+					<SignUpScreen />
+				) : (
+					<Switch>
+						<Route path='/' exact>
+							<Header />
+							<Home />
+							<Footer />
+						</Route>
+						<Route path='/CheckoutProduct'>
+							<Header />
+							<Checkout />
+							<Footer />
+						</Route>
+						<Route path='/login' exact>
+							<LoginScreen />
+						</Route>
+						<Route path='/register' exact>
+							<SignUpScreen />
+						</Route>
+						<Route path='/Payment'>
+							<Header />
+							<Payment />
+							<Footer />
+						</Route>
 
-					<Route path='/CheckoutProduct'>
-						<Checkout />
-					</Route>
-				</Switch>
+						<Route path='/order'>
+							<Header />
+							<Order />
+							<Footer />
+						</Route>
+					</Switch>
+				)}
 			</Router>
 		</AppWrapper>
 	);
