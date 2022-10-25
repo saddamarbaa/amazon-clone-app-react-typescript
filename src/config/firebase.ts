7f4502905b01700c866edde018ab57580e84edb5
saddamarbaa <saddam@rushowl.sg>
2022-10-26 01:24:14 +0700

    chore: add firebase keys and test build

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -15,12 +15,18 @@ import { getFirestore } from 'firebase/firestore'
 const googleProvider = new GoogleAuthProvider()
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAIN_SENDER_ID,
-	appId: process.env.REACT_APP_APPID,
+	 apiKey: "AIzaSyDYMtLgrfLKwYXGbBiSKloyzN5c8QiDZBo",
+
+  authDomain: "ecommerce-a6301.firebaseapp.com",
+
+  projectId: "ecommerce-a6301",
+
+  storageBucket: "ecommerce-a6301.appspot.com",
+
+  messagingSenderId: "86853033296",
+
+  appId: "1:86853033296:web:b857d9f1f2354a71cda055"
+
 }
 
 // Initialize Firebase
